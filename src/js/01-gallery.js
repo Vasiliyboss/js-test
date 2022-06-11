0cd0cfb008ad809071c959ee225a09db5702f877
Vasiliy <mazurboss2016@gmail.com>
2022-06-11 23:01:07 +0300

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const createGallery = galleryItems.map(image => 
+  `<div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>`
+).join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', createGallery);
+// galleryContainer.addEventListener('click', onImageClick)
+// function onImageClick(event) { 
+//   event.preventDefault();
+//     if (event.target.classList.contains('gallery')) { 
+//         return;
+//     }
+//   const sourceImage = event.target.dataset.source;
+
+//   const instance = basicLightbox.create(`<img class="modal-img" src="${sourceImage}">`, {
+//     onshow: (instance) => window.addEventListener('keydown', onEscPress),
+//     onclose: (instance) => window.removeEventListener('keydown', onEscPress),
+//   });
+//   instance.show();
+
+//   function onEscPress(event) { 
+//     if (event.code === 'Escape') { 
+//       instance.close();
+//       window.removeEventListener('keydown', onEscPress);
+//     }
+    
+//   }
+// }
